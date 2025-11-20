b4dbe2a94e9d9c1bafbe1983ae9ede480a4d50b2
Adrian <adi.ovi.petre@gmail.com>
2025-11-20 20:24:02 +0200

    Integrated Heroku handling

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const distPath = path.join(__dirname, 'dist/suite-sync-ui/browser');
+
+app.use(express.static(distPath));
+
+app.use((req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
